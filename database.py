65dee5f7f39086f6bcf5077b716cb203315b8d91
Matthew Bernhardt <mjbernha@mit.edu>
2015-11-01 09:04:27 -0500

    Updated shims
    Code formatting, adding detail to shims for database and log content

--- database.py
+++ database.py
@@ -1,23 +1,30 @@
 # Import libraries
 import mysql.connector
-import connection
+import connection as connection
 
 cnx = ''
 cursor = ''
 
-def connect():
-  global cnx, cursor
 
-  cnx = mysql.connector.connect(user=connection.u, password=connection.p, host=connection.h, database=connection.d)
-  cursor = cnx.cursor()
+def connect():
+    global cnx, cursor
+    cnx = mysql.connector.connect(user=connection.u, password=connection.p, host=connection.h, database=connection.d)
+    cursor = cnx.cursor(buffered=True)
+
 
 def disconnect():
-  global cnx, cursor
+    global cnx, cursor
+    cursor.close()
+    cnx.close()
 
-  cursor.close()
-  cnx.close()
 
-def query(query):
-  global cnx, cursor
-  cursor.execute(query, ())
-  return cursor
+def query(query, params):
+    global cnx, cursor
+    cursor.execute(query, params)
+    cnx.commit()
+    return cursor
+
+
+def warnings():
+    global cnx, cursor
+    return cursor.fetchwarnings()
--- log.py
+++ log.py
@@ -1,16 +1,19 @@
 log = ''
 
+
 def start(logFile):
-  global log
-  log = open(logFile,'w')
-  log.write('Log start\n')
+    global log
+    log = open(logFile, 'w')
+    log.write('Log start\n')
+
 
 def message(msg):
-  global log
-  log.write(msg)
-  log.write('\n')
+    global log
+    log.write(msg)
+    log.write('\n')
+
 
 def end():
-  global log
-  log.write('Log end\n')
-  log.close()
+    global log
+    log.write('Log end\n')
+    log.close()
